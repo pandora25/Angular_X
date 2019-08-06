ea0161211cc8d9585a0516b80ff8a2b739f247e0
michael yalda <MCYALDA@yahoo.com>
2019-08-06 19:31:44 -0400

    add chatbot.

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalsComponent } from './components/modals/modals.component';
+import { ChatBotComponent } from './components/chat-bot/chat-bot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModalsComponent
+    ModalsComponent,
+    ChatBotComponent
   ],
   imports: [
     BrowserModule,
